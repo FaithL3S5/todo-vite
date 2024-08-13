0c6a295e49626b5d0b0b58657134b1502d4c6893
Faith <73940605+FaithL3S5@users.noreply.github.com>
2024-08-13 14:19:58 +0700

    force light mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  ColorModeScript,
+  ThemeConfig,
+  extendTheme,
+} from "@chakra-ui/react";
 import App from "./App.tsx";
+
+const config: ThemeConfig = {
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
+
+const customTheme = extendTheme({ config });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
+    <ChakraProvider theme={customTheme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
